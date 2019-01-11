051068803e0c816bf22ca20e6375e23e3004a6fb
Gaurav Khanna <iamgauravkhanna@gmail.com>
2019-01-11 16:34:57 +0530

    New Changes

--- learning-webdriver/basic_script_1.py
+++ learning-webdriver/basic_script_1.py
@@ -1,11 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import sys 
 
 #https://www.techbeamers.com/selenium-webdriver-python-tutorial/
 
 driver = webdriver.Firefox(executable_path="C:\\office\\code\\workspace-python\\automation-repository-python\\drivers\\windows\\geckodriver-v0.23.0-win64\\geckodriver.exe")
 
-driver.get("http://www.google.com")
+# Assigning URL to variable 'baseUrl'
+baseUrl = "http://book.theautomatedtester.co.uk"
 
+# Open the link
+driver.get(baseUrl)
+
+# Maximize browser window
+driver.maximize_window()
+
+# Get Page Title
+title = driver.title
+
+# Print Page Title
+print('Page Title :' + title)
+
+# Quit Driver
 driver.quit()
